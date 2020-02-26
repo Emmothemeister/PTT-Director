d821b3fa757a5635747cd7ba0bf9a9f7a4064cf4
Emmo <490665325@qq.com>
2020-02-26 15:22:55 +0000

    second commit

--- src/Model/PTTD.java
+++ src/Model/PTTD.java
@@ -1,5 +1,27 @@
 package Model;
 
-public class PTTD {
+import java.util.ArrayList;
 
+public class PTTD extends Staff{
+	private String SName;
+	private	String UName;
+	private String password;
+	private MailBox mailbox;
+	private String contact;
+	private ArrayList<Request> requestBox;
+	private ArrayList<LogginHistory> logginHistory;
+	
+	
+	public PTTD(String n, String u, String p, String c){
+		super(); // get a automatically increment SID
+		SName = n;
+		UName = u;
+		password = p;
+		contact = c;
+		mailbox = new MailBox();
+		requestBox = new ArrayList<Request>();
+		logginHistory = new ArrayList<LogginHistory>();
+		
+		
+	}
 }
