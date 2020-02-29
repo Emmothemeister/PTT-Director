e16289b1b9def6d50fb0f27ab6876aeaf2eb5b86
Emmo <490665325@qq.com>
2020-02-29 17:09:38 +0000

    fourth commit

--- src/Model/CourseD.java
+++ src/Model/CourseD.java
@@ -7,17 +7,15 @@ public class CourseD extends Staff{
 	private int courseID;
 	private ArrayList<Mail> mailBox;
 	private int contact;
-	private ArrayList<LogginHistory> logginHistory;
+	
 	private ArrayList<Request> applyingRequest;
 	private ArrayList<Request> rejectedRequest;
 	private ArrayList<Request> approvedRequest;
 	private ArrayList<Teacher> supervisedTeachers;
 	
-	public CourseD(String s, String u, String p, int c, int o) {
-		super(s,u,p);
+	public CourseD(String s, String u, String p, int o, int c) {
+		super(s,u,p,o);
 		courseID = c;
-		contact = o;
-		logginHistory = new ArrayList<LogginHistory>();
 		applyingRequest = new ArrayList<Request>();
 		rejectedRequest = new ArrayList<Request>();
 		approvedRequest = new ArrayList<Request>();
--- src/Model/Mail.java
+++ src/Model/Mail.java
@@ -4,14 +4,16 @@ public class Mail {
 	private Time sendTime;
 	private int senderID;
 	private int receiverID;
+	private String title;
 	private String content;
 	private boolean status;
 	
 	
-	public Mail(Time t, int s, int r, String c) {
+	public Mail(Time t, int s, int r,String tl, String c) {
 		sendTime = t;
 		senderID = s;
 		receiverID = r;
+		title = tl;
 		content = c;
 		status = false; //status is always false before the mail has been read
 	}
